e58aec3f0262bfc3c905cd2fc0898457d916254c
RewanElsonbaty <rewanelsonbaty735@gmail.com>
2024-05-03 22:49:00 +0300

    bella app

---
+++ lib/features/home/views/home_screen.dart
@@ -0,0 +1,64 @@
+import 'package:bella/core/utils/app_images.dart';
+import 'package:bella/features/favourites/views/favorite_screen.dart';
+import 'package:bella/features/home/views/widgets/home_body.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex =  0;
+
+
+  List<Widget> screens  = [
+    // current index  = 0
+    const HomeBody() ,
+    const FavoriteScreen()
+
+  ] ;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.scaffoldColor,
+      appBar:AppBar(backgroundColor: AppColors.scaffoldColor,
+        centerTitle:true ,
+        title: Image.asset(AppImages.bellaLogo,
+          height: MediaQuery.of(context).size.height*0.04,
+        ),
+      ) ,
+      drawer: const Drawer(),
+      bottomNavigationBar: BottomNavigationBar(
+
+        onTap: (bottomIndex){
+
+          print(bottomIndex.toString());
+          setState(() {
+            currentIndex = bottomIndex;
+          });
+        },
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        items: const [
+
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "Favorite"    ,
+          ),
+
+        ],
+
+      ),
+      body:screens[currentIndex],
+      // body: ,
+    );
+  }
+}
---
+++ lib/features/home/views/widgets/home_body.dart
@@ -0,0 +1,35 @@
+import 'package:bella/features/home/data/models/food_model.dart';
+import 'package:bella/features/home/views/widgets/banner_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'food_home_widget.dart';
+
+class HomeBody extends StatelessWidget {
+  const HomeBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: CustomScrollView(
+        slivers: [
+          const SliverToBoxAdapter(
+            child:         CustomBanner(),
+          )  ,
+          SliverGrid.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+
+              crossAxisCount: 2
+
+          ),
+              itemCount: foods.length,
+              itemBuilder: (c,index) {
+                return   HomeFoodWidget(
+                  index:index ,
+                );
+              })
+
+        ],
+      ),
+    );
+  }
+}
